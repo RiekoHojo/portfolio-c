680a26f1f4184ae7aad4d4cb619ad8f8de861132
riekohojo <lieco1031@gmail.com>
2024-01-26 00:24:08 +0900

    0125

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+console.log("test")
+
+
+$(".top-hamburger-icon").on('click', function(){
+  if($(this).hasClass('active')){
+    $('.top-nav-sp').removeClass('open');
+    $(this).removeClass('active');
+  } else {
+    $('.top-nav-sp').addClass('open');
+    $(this).addClass('active');
+  }
+});
+
