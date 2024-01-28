5201b35c04c038e9c3a094689addd764f05b7d7f
riekohojo <lieco1031@gmail.com>
2024-01-29 01:51:20 +0900

    0128

--- JS/main.js
+++ JS/main.js
@@ -12,7 +12,7 @@ $(".top-hamburger-icon").on('click', function(){
     $('body').addClass('no-scroll');
   }
 });
-  
+
 /*トップへ戻るボタン*/  
 $(function() {
   var $btn = $('.box-container');
@@ -22,19 +22,73 @@ $(function() {
   $btn.hide();
 
   function checkIfInView() {
-    var windowHeight = $(window).height();
-    var scrollPos = $(window).scrollTop();
-    var triggerPos = $trigger.offset().top;
+      var windowHeight = $(window).height();
+      var scrollPos = $(window).scrollTop();
+      var triggerPos = $trigger.offset().top;
 
-    if (scrollPos + windowHeight > triggerPos) {
+      if (scrollPos + windowHeight > triggerPos) {
       $btn.fadeIn(200);
-    }
+      }
 
-    clearTimeout(scrollTimer);
-    scrollTimer = setTimeout(function() {
+      clearTimeout(scrollTimer);
+      scrollTimer = setTimeout(function() {
       $btn.fadeOut(200);
-    }, 2000);
+      }, 2000);
   }
-
+  
   $(window).on('scroll resize', checkIfInView);
+
+  $btn.click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 500);
+  });
 });
+
+/*slick slide*/
+$('.slider').not('.slick-initialized').slick({
+  autoplay: true,
+  autoplaySpeed: 3000,
+  speed:1000,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  prevArrow: '<div class="slick-prev"></div>',
+  nextArrow: '<div class="slick-next"></div>',
+});
+
+$('.slider').not('.slick-initialized').slick({
+dots: true,
+  pauseOnFocus: false,
+  pauseOnHover: false,
+  pauseOnDotsHover: false,
+});
+
+/*言語プルダウン*/
+$(document).ready(function() {
+  $('.top-language').click(function(e) {
+    $('.top-language-box').toggle();
+    e.stopPropagation();
+  });
+
+  $('.top-language-box-item').click(function() {
+    $('.top-language-box-item').removeClass('highlight');
+    $('.selected-language').removeClass('highlight');
+
+    var selectedLanguage = $(this).text();
+    $('.selected-language').text(selectedLanguage);
+
+    $('.selected-language').addClass('highlight');
+
+    $('.top-language-box').hide();
+  });
+
+  $(document).click(function() {
+    $('.top-language-box').hide();
+    $('.top-language-box-item').removeClass('highlight');
+    $('.selected-language').removeClass('highlight');
+  });
+});
+
+
