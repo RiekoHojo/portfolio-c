f1c6cbe77f89e1c5677d78cec2b2e9bb21126355
riekohojo <lieco1031@gmail.com>
2024-03-04 00:37:28 +0900

    0303

--- JS/main.js
+++ JS/main.js
@@ -48,20 +48,22 @@ $(function() {
 });
 
 /*slick slide*/
-$('.slider').slick({
-  autoplay: true,
-  autoplaySpeed: 3000,
-  speed: 1000,
-  infinite: true,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: true,
-  prevArrow: '<div class="slick-prev"></div>',
-  nextArrow: '<div class="slick-next"></div>',
-  dots: true, 
-  pauseOnFocus: false,
-  pauseOnHover: false,
-  pauseOnDotsHover: false
+$(function() {
+  $('.slider').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 1000,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: '<div class="slick-prev"></div>',
+    nextArrow: '<div class="slick-next"></div>',
+    dots: true, 
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    pauseOnDotsHover: false
+  }
 });
 
 
