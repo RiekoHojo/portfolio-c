572b9847b697b2d29c9a5f743b457f46488e7a20
riekohojo <lieco1031@gmail.com>
2024-04-27 16:40:18 +0900

    0427

--- JS/main.js
+++ JS/main.js
@@ -46,7 +46,7 @@ $(function() {
 });
 
 /*slick slide*/
-/*$(function() {
+$(function() {
   var sliderOptions = {
     autoplay: true,
     autoplaySpeed: 3000,
@@ -61,42 +61,6 @@ $(function() {
     pauseOnFocus: false,
     pauseOnHover: false,
     pauseOnDotsHover: false
-  };
-
-  $(window).on('resize', function() {
-    if ($(window).width() <= 767) {
-      sliderOptions.fade = false; // SPサイズフェードしない
-    } else {
-      sliderOptions.fade = true; // PCサイズフェード
-    }
-  }).resize(); 
-
-  $('.slider').slick(sliderOptions);
-});*/
-$(function() {
-  var sliderOptions = {
-    autoplay: true,
-    autoplaySpeed: 3000,
-    speed: 1000,
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-    prevArrow: '<div class="slick-prev"></div>',
-    nextArrow: '<div class="slick-next"></div>',
-    dots: true,
-    pauseOnFocus: false,
-    pauseOnHover: false,
-    pauseOnDotsHover: false,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false, // 矢印を非表示
-          fade: false
-        }
-      }
-    ]
   };
 
   $(window).on('resize', function() {
